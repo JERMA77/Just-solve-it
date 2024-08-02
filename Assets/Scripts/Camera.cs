0cd1c2c5c97a556a9c5bd192f2147a65cdcaeeef
Морозов Даниил <dany2005morozov@mail.ru>
2024-08-02 16:44:18 +0300

    Add player movement and camera follow

---
+++ Assets/Scripts/Camera.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera : MonoBehaviour
+{
+    public Transform player;
+    void FixedUpdate()
+    {
+        transform.position = new Vector3(player.position.x + 6, transform.position.y, -10);
+    }
+}
--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -6,27 +6,18 @@ public class Movement : MonoBehaviour
 {
     public float speed;
     private Rigidbody2D rb;
-    private Vector2 moveVector;
-
+    private Vector2 direction;
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        direction = new Vector2(1 * speed, rb.velocity.y);
+        //rb.AddForce(direction, ForceMode2D.Impulse);
     }
 
     
-    void Update()
+    void FixedUpdate()
     {
-        moveVector.x = Input.GetAxis("Horizontal");
-        moveVector.y = Input.GetAxis("Vertical");
-
-        if (moveVector.x > 0)
-        {
-            rb.MovePosition(rb.position + moveVector * speed * Time.deltaTime);
-        }
-        else if (moveVector.x > 0)
-        {
-            rb.MovePosition(rb.position - moveVector * speed * Time.deltaTime);
-        }
+        rb.velocity = direction;
     }
 }
